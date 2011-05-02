82124db9bedab61a9ae827fc67a168025887646d
atlas-2192 <sunstar962090@gmail.com>
2011-05-01 20:49:20 -0700

    library

---
+++ web/Formal-Language-Algorithms/library/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "gramIO.h"
+#include "lib.h"
+
+
+using namespace std;
+int main(){
+
+
+
+	set<char*> grammer[4];
+
+
+	parseGrammer("grammer.txt", grammer);	
+/*	parseGrammer("grammer.txt", grammer);	
+	parseGrammer("grammer.txt", grammer);	
+	parseGrammer("grammer.txt", grammer);	
+	parseGrammer("grammer.txt", grammer);	
+	parseGrammer("grammer.txt", grammer);	
+	parseGrammer("grammer.txt", grammer);	
+	parseGrammer("grammer.txt", grammer);	
+	parseGrammer("grammer.txt", grammer);	
+	parseGrammer("grammer.txt", grammer);	
+	parseGrammer("grammer.txt", grammer);	
+	parseGrammer("grammer.txt", grammer);	
+	parseGrammer("grammer.txt", grammer);	
+	parseGrammer("grammer.txt", grammer);	
+	parseGrammer("grammer.txt", grammer);	
+	parseGrammer("grammer.txt", grammer);	
+	parseGrammer("grammer.txt", grammer);	
+	parseGrammer("grammer.txt", grammer);	
+	parseGrammer("grammer.txt", grammer);	*/
+}
