1d18ba7bf569f08c62f1a1344c295c81638d192e
atlas-2192 <sunstar962090@gmail.com>
2011-05-30 10:58:01 -0700

    Updated the main shell

--- Chapter5_6/main/chap5_6.h
+++ Chapter5_6/main/chap5_6.h
@@ -1,10 +1,11 @@
 #include <iostream>
 
 
-
+//Remove non-determinism
 void alg5_6(char* pathIn, char* pathOut);
 
+//Determin equivalant states
 void alg5_7(char* pathIn, char* pathOut);
 
-// FA -> RE
+//Finite State to a regular expression
 void alg6_2(char* pathIn, char* pathOut);
